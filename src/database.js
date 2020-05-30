4c430115942b6664ce2095aec3290003356f6f8a
unknown <miltonmercado92@gmail.com>
2020-05-30 17:26:50 -0600

    Viersion 1.0.0.2

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const URI = "mongodb://localhost/archivos";
 
 const uri = "mongodb+srv://archivos:58385772david@cluster0-va3kg.mongodb.net/test?retryWrites=true&w=majority";
 
-mongoose.connect(uri, {
+mongoose.connect(URI, {
         useNewUrlParser: true
     })
     .then(db => console.log("db conectada"))
