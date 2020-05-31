0c24423aee51905933af066fe3bf9718a23bfb1c
unknown <miltonmercado92@gmail.com>
2020-05-31 08:00:26 -0600

    Version 1.0.0.6

--- src/database.js
+++ src/database.js
@@ -6,9 +6,9 @@ const uri = "mongodb+srv://archivos:58385772david@cluster0-va3kg.mongodb.net/tes
 
 const con = "mongodb://ulh7uxwmzgh9karxrdsf:5MaHW9DpBN7IXOrmhX1R@bxybxtj8smatat5-mongodb.services.clever-cloud.com:27017/bxybxtj8smatat5";
 
-
 mongoose.connect(con, {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
     .then(db => console.log("db conectada"))
     .catch(err => console.log('error'));
