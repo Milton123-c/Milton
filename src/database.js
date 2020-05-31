5bde27d445d4ad470fe465e426e0b2d9c9a6dfca
unknown <miltonmercado92@gmail.com>
2020-05-30 18:26:53 -0600

    Version 1.0.0.4

--- src/database.js
+++ src/database.js
@@ -4,6 +4,7 @@ const URI = "mongodb://localhost/archivos";
 
 const uri = "mongodb+srv://archivos:58385772david@cluster0-va3kg.mongodb.net/test?retryWrites=true&w=majority";
 
+
 mongoose.connect(URI, {
         useNewUrlParser: true
     })
